1e8503c8da2874e3e07efe5d4d097bc65565878a
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-09-29 23:27:55 +0545

    chore: file structuring and view updates

---
+++ WalkAR/Core/View/Home/View/AR/View/ARCameraView.swift
@@ -0,0 +1,18 @@
+//
+//  ARCameraView.swift
+//  WalkAR
+//
+//  Created by Prashanna Rajbhandari on 29/09/2023.
+//
+
+import SwiftUI
+
+struct ARCameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARCameraView()
+}
