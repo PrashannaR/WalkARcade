eebf9d76812180a9be69a76077a978dcbaf0c72e
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-09-29 23:30:21 +0545

    feat: ui updates and fixes

---
+++ WalkAR/Core/View/Home/View/AR/Model/Model.swift
@@ -0,0 +1,33 @@
+//
+//  Model.swift
+//  WalkAR
+//
+//  Created by Prashanna Rajbhandari on 29/09/2023.
+//
+
+import Combine
+import Foundation
+import RealityKit
+import UIKit
+
+class Model {
+    var modelName: String
+    var image: UIImage
+    var modelEntity: ModelEntity?
+
+    private var cancellable: AnyCancellable?
+
+    init(modelName: String) {
+        self.modelName = modelName
+        image = UIImage(named: modelName) ?? UIImage(named: "toy_biplane_idle")!
+
+        let filename = modelName + ".usdz"
+        cancellable = ModelEntity.loadModelAsync(named: filename).sink(receiveCompletion: { _ in
+            print("Unable to load model entity for \(self.modelName)")
+        }, receiveValue: { modelEntity in
+            self.modelEntity = modelEntity
+            print("sucessfully loaded model entity for \(self.modelName)")
+        })
+    }
+}
+  
--- WalkAR/Core/View/Home/View/AR/View/ARCameraView.swift
+++ WalkAR/Core/View/Home/View/AR/View/ARCameraView.swift
@@ -5,14 +5,112 @@
 //  Created by Prashanna Rajbhandari on 29/09/2023.
 //
 
+import ARKit
+import FocusEntity
+import RealityKit
 import SwiftUI
 
 struct ARCameraView: View {
+    let modelToBePlaced: Model
+
+    @State private var navigateToHome = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom, content: {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Points: \(PointsManager.shared.points)")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                    Steps()
+                }.padding()
+                ARViewContainer(modelConfirmedForPlacement: modelToBePlaced)
+            }
+
+        })
+        .background(
+            NavigationLink("", destination: HomeView(), isActive: $navigateToHome)
+                .opacity(0) // Hide the navigation link
+        )
+        .onReceive(NotificationCenter.default.publisher(for: .pointsIncreased)) { _ in
+
+            navigateToHome = true
+        }
+    }
+}
+
+// MARK: ARView Container
+
+struct ARViewContainer: UIViewRepresentable {
+    var modelConfirmedForPlacement: Model?
+
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
+        arView.isUserInteractionEnabled = true
+
+        arView.enableObjectRemoval()
+
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+
+        arView.session.run(config)
+
+        _ = FocusEntity(on: arView, focus: .classic)
+
+        return arView
+    }
+
+    func updateUIView(_ uiView: ARView, context: Context) {
+        if let model = modelConfirmedForPlacement {
+            if let modelEntity = model.modelEntity {
+                print("adding \(model.modelName) to the scene")
+                let anchorEntity = AnchorEntity(plane: .any)
+                anchorEntity.name = "ObjectAnchor"
+                modelEntity.generateCollisionShapes(recursive: true)
+
+                anchorEntity.addChild(modelEntity
+                    .clone(recursive: true)
+                )
+                uiView.scene.addAnchor(anchorEntity)
+            } else {
+                print("unable to load \(model.modelName) to the scene")
+            }
+        }
+    }
+}
+
+// MARK: remove the object from the scene
+
+extension ARView {
+    func enableObjectRemoval() {
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        addGestureRecognizer(longPressRecognizer)
+    }
+
+    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer) {
+        let location = recognizer.location(in: self)
+
+        if let entity = entity(at: location) {
+            if let anchorEntity = entity.anchor, anchorEntity.name == "ObjectAnchor" {
+                anchorEntity.removeFromParent()
+                print("removed the object ")
+                PointsManager.shared.increasePoints()
+                NotificationCenter.default.post(name: .pointsIncreased, object: nil)
+            }
+        }
     }
 }
 
 #Preview {
-    ARCameraView()
+    ARCameraView(modelToBePlaced: Model(modelName: ""))
+}
+
+extension Notification.Name {
+    static let pointsIncreased = Notification.Name("PointsIncreased")
 }
