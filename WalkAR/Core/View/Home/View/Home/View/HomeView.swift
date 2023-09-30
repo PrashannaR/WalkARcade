3ee9a139df0e035576a98f766996546f080d7efd
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-10-01 04:44:15 +0545

    feat: ui updates

--- WalkAR/Core/View/Home/View/Home/View/HomeView.swift
+++ WalkAR/Core/View/Home/View/Home/View/HomeView.swift
@@ -8,28 +8,11 @@
 
 import SwiftUI
 
-struct LeaderboardData: Codable {
-    let username: String
-    let points: Int
-    let steps: Int
-}
-
-struct User: Codable, Identifiable {
-    let id = UUID()
-    let username: String
-    let points: Int
-    let steps: Int
-}
-
-
 struct HomeView: View {
     @StateObject var vm = ContentViewModel()
     @State var model: Model = Model(modelName: "")
     
-    @State private var myUser: LeaderboardData = LeaderboardData(username: "", points: 0, steps: 0)
-
-    @State private var users: [User] = []
-
+   
     var body: some View {
         NavigationView {
             ZStack {
@@ -38,52 +21,15 @@ struct HomeView: View {
                 ScrollView {
                     
                     VStack(alignment: .leading) {
-                        //personal details
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text("\(myUser.username)")
-                                    .font(.title3)
-                                    .fontWeight(.bold)
-                                Text("Points: \(myUser.points)")
-                                    .foregroundStyle(Color.black.opacity(0.3))
-                                    .font(.headline)
-                            }
-                            Spacer()
-                            Text("Steps: \(myUser.steps)")
-                                .foregroundStyle(Color.black.opacity(0.3))
-                                .font(.headline)
-                        }
-                        .foregroundColor(Color.black)
                         
-                        //leaderboard
-                        Text("Leaderboard")
-                            .font(.title)
-                            .foregroundStyle(.blue)
-                        List(users) { user in
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text("\(user.username)")
-                                        .font(.title3)
-                                        .fontWeight(.bold)
-                                    Text("Points: \(user.points)")
-                                        .foregroundStyle(Color.black.opacity(0.3))
-                                        .font(.headline)
-                                }
-                                Spacer()
-                                Text("Steps: \(user.steps)")
-                                    .foregroundStyle(Color.black.opacity(0.3))
-                                    .font(.headline)
-                            }
-                            .foregroundColor(Color.black)
-                        }
-
+                        Leaderboard()
                         
                         
                         Spacer()
 
                         NavigationLink {
-                            MyAreaMap()
-                            //Leaderboard()
+                           // MyAreaMap()
+                            Leaderboard()
 
                         } label: {
                             CustomButton(imageName: "map.fill", title: "Maps")
@@ -101,6 +47,9 @@ struct HomeView: View {
                             CustomButton(imageName: "figure.walk", title: "Steps")
                         }
                     }
+                    .background {
+                        Color.white
+                    }
                 }.padding()
             }
                 .onAppear {
@@ -108,8 +57,7 @@ struct HomeView: View {
                         model = vm.generateRandomModel()
                         print("generated model: ", model.modelName)
                     }
-                    loadData()
-                    fetchData()
+        
                 }
                 .onAppear {
                     NotificationCenter.default.addObserver(
@@ -147,55 +95,5 @@ extension HomeView {
         }
     }
     
-    func loadData() {
-        guard let url = URL(string: "http://192.168.101.121:8000/leaderboard/prashannar/") else { return }
 
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let data = data {
-                do {
-                    let decoder = JSONDecoder()
-                    let decodedData = try decoder.decode(LeaderboardData.self, from: data)
-                    DispatchQueue.main.async {
-                        self.myUser = decodedData
-                    }
-                } catch {
-                    print("Error decoding data: \(error)")
-                }
-            }
-        }.resume()
-    }
-    
-    func fetchData() {
-        let config = URLSessionConfiguration.default
-        config.allowsExpensiveNetworkAccess = true
-        config.allowsConstrainedNetworkAccess = true
-        config.waitsForConnectivity = true
-
-        _ = URLSession(configuration: config)
-        
-        guard let url = URL(string: "http://192.168.101.121:8000/leaderboard/") else {
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("csrftoken=hFTtUxKrUhxPnge7Hu0QnIrx3KCMzfge; sessionid=pvadi9d4kj4yw69mdg1afad8zzee2sn1", forHTTPHeaderField: "Cookie")
-
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                do {
-                    let decodedData = try JSONDecoder().decode([String: [User]].self, from: data)
-                    if let usersArray = decodedData["users"] {
-                        DispatchQueue.main.async {
-                            users = usersArray
-                        }
-                    }
-                } catch {
-                    print("Error decoding JSON: \(error)")
-                }
-            } else if let error = error {
-                print("Error: \(error.localizedDescription)")
-            }
-        }.resume()
-    }
 }
--- WalkAR/Core/View/Leaderboard/Leaderboard.swift
+++ WalkAR/Core/View/Leaderboard/Leaderboard.swift
@@ -1,117 +1,147 @@
-////
-////  Leaderboard.swift
-////  WalkAR
-////
-////  Created by Prashanna Rajbhandari on 01/10/2023.
-////
 //
+//  Leaderboard.swift
+//  WalkAR
 //
-//import SwiftUI
+//  Created by Prashanna Rajbhandari on 01/10/2023.
 //
-//
-//struct User: Codable, Identifiable {
-//    let id = UUID()
-//    let username: String
-//    let points: Int
-//    let steps: Int
-//}
-//
-//struct LeaderboardData: Codable {
-//    let username: String
-//    let points: Int
-//    let steps: Int
-//}
-//
-//
-//struct Leaderboard: View {
-//    @State private var users: [User] = []
-//    
-//    @State private var myUser: LeaderboardData = LeaderboardData(username: "", points: 0, steps: 0)
-//
-//    
-//    var body: some View {
-//        NavigationView {
-//            ZStack{
-//                Color.white.ignoresSafeArea()
-//                
-//                VStack(alignment: .leading){
-//                    VStack(alignment: .leading) {
-//                        Text("Username: \(myUser.username)")
-//                        Text("Points: \(myUser.points)")
-//                        Text("Steps: \(myUser.steps)")
-//                    }
-//                    List(users) { user in
-//                        VStack(alignment: .leading) {
-//                            Text("Username: \(user.username)")
-//                            Text("Points: \(user.points)")
-//                            Text("Steps: \(user.steps)")
-//                        }
-//                    }
-//                }.foregroundStyle(Color.black)
-//            }
-//            .padding()
-//            .onAppear {
-//                fetchData()
-//                loadData()
-//            }
-//        }
-//    }
-//
-//    func fetchData() {
-//        let config = URLSessionConfiguration.default
-//        config.allowsExpensiveNetworkAccess = true
-//        config.allowsConstrainedNetworkAccess = true
-//        config.waitsForConnectivity = true
-//
-//        _ = URLSession(configuration: config)
-//        
-//        guard let url = URL(string: "http://192.168.101.121:8000/leaderboard/") else {
-//            return
-//        }
-//
-//        var request = URLRequest(url: url)
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("csrftoken=hFTtUxKrUhxPnge7Hu0QnIrx3KCMzfge; sessionid=pvadi9d4kj4yw69mdg1afad8zzee2sn1", forHTTPHeaderField: "Cookie")
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            if let data = data {
-//                do {
-//                    let decodedData = try JSONDecoder().decode([String: [User]].self, from: data)
-//                    if let usersArray = decodedData["users"] {
-//                        DispatchQueue.main.async {
-//                            users = usersArray
-//                        }
-//                    }
-//                } catch {
-//                    print("Error decoding JSON: \(error)")
-//                }
-//            } else if let error = error {
-//                print("Error: \(error.localizedDescription)")
-//            }
-//        }.resume()
-//    }
-//    
-//    func loadData() {
-//        guard let url = URL(string: "http://192.168.101.121:8000/leaderboard/prashannar/") else { return }
-//
-//        URLSession.shared.dataTask(with: url) { data, _, error in
-//            if let data = data {
-//                do {
-//                    let decoder = JSONDecoder()
-//                    let decodedData = try decoder.decode(LeaderboardData.self, from: data)
-//                    DispatchQueue.main.async {
-//                        self.myUser = decodedData
-//                    }
-//                } catch {
-//                    print("Error decoding data: \(error)")
-//                }
-//            }
-//        }.resume()
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Leaderboard()
-//    }
-//}
+
+import SwiftUI
+
+struct User: Codable, Identifiable {
+    let id = UUID()
+    let username: String
+    let points: Int
+    let steps: Int
+}
+
+struct LeaderboardData: Codable {
+    let username: String
+    let points: Int
+    let steps: Int
+}
+
+struct Leaderboard: View {
+    @State private var users: [User] = []
+
+    @State private var myUser: LeaderboardData = LeaderboardData(username: "", points: 0, steps: 0)
+
+    var body: some View {
+        ZStack{
+            
+            NavigationView {
+                ZStack {
+                    Color.white.ignoresSafeArea()
+
+                    VStack(alignment: .leading) {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("\(myUser.username)")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                Text("Points: \(myUser.points)")
+                                    .foregroundStyle(Color.black.opacity(0.3))
+                                    .font(.headline)
+                            }
+                            Spacer()
+                            Text("Steps: \(myUser.steps)")
+                                .foregroundStyle(Color.black.opacity(0.3))
+                                .font(.headline)
+                        }
+                        .foregroundColor(Color.black)
+                        
+                        Text("Leaderboard")
+                            .font(.title2)
+                            .bold()
+                            .foregroundStyle(.blue)
+
+                        List(users) { user in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("\(user.username)")
+                                        .font(.title3)
+                                        .fontWeight(.bold)
+                                    Text("Points: \(user.points)")
+                                        .foregroundStyle(Color.black.opacity(0.3))
+                                        .font(.headline)
+                                }
+                                Spacer()
+                                Text("Steps: \(user.steps)")
+                                    .foregroundStyle(Color.black.opacity(0.3))
+                                    .font(.headline)
+                            }
+                            .foregroundColor(Color.black)
+                            .listRowBackground(Color.white)
+                        }.listStyle(.plain)
+                    }
+                    .background {
+                        Color.white
+                    }
+                }
+                .padding()
+                .onAppear {
+                    fetchData()
+                    loadData()
+                }
+            }
+        }
+
+    }
+
+    func fetchData() {
+        let config = URLSessionConfiguration.default
+        config.allowsExpensiveNetworkAccess = true
+        config.allowsConstrainedNetworkAccess = true
+        config.waitsForConnectivity = true
+
+        _ = URLSession(configuration: config)
+
+        guard let url = URL(string: "http://192.168.101.121:8000/leaderboard/") else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("csrftoken=hFTtUxKrUhxPnge7Hu0QnIrx3KCMzfge; sessionid=pvadi9d4kj4yw69mdg1afad8zzee2sn1", forHTTPHeaderField: "Cookie")
+
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let data = data {
+                do {
+                    let decodedData = try JSONDecoder().decode([String: [User]].self, from: data)
+                    if let usersArray = decodedData["users"] {
+                        DispatchQueue.main.async {
+                            users = usersArray
+                        }
+                    }
+                } catch {
+                    print("Error decoding JSON: \(error)")
+                }
+            } else if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+
+    func loadData() {
+        guard let url = URL(string: "http://192.168.101.121:8000/leaderboard/prashannar/") else { return }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    let decodedData = try decoder.decode(LeaderboardData.self, from: data)
+                    DispatchQueue.main.async {
+                        self.myUser = decodedData
+                    }
+                } catch {
+                    print("Error decoding data: \(error)")
+                }
+            }
+        }.resume()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Leaderboard()
+    }
+}
